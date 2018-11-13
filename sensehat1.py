c875be3b6e98ae6114dfc532a0b73f7c22cbbf35
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2018-11-13 13:04:58 +0100

    Create sensehat1.py
    mit starter kode

---
+++ sensehat1.py
@@ -0,0 +1,27 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+green = (0,255,0)
+blue  = (0,0,255)
+red = (255,0,0)
+nothing = (0,0,0)
+
+sense.clear()
+
+
+x = 1
+y = 0
+
+while (y<8):
+  sense.set_pixel(x,y,green)
+  time.sleep(0.1)
+  sense.set_pixel(x,y,blue)
+  time.sleep(0.1)
+  sense.set_pixel(x,y,red)
+  if y >= 3:
+    sense.set_pixel(x,y-3,nothing)
+  y = y+1
+  
+  
